b86c50642a565a23c641f93b159a78c6b6be3dc8
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-10-02 23:57:05 +0700

    Update tf-init.sh

--- terraform/prepare/tf-init.sh
+++ terraform/prepare/tf-init.sh
@@ -6,7 +6,7 @@ ID_SA=`yc iam service-account list | grep 'sa-robot' | awk '{print $2}'`
 yc resource-manager folder add-access-binding --id b1g0jjnic75phgkajfgn --role editor --service-account-id $ID_SA
 yc resource-manager folder add-access-binding --id b1g0jjnic75phgkajfgn --role storage.admin --service-account-id $ID_SA
 yc resource-manager folder add-access-binding --id b1g0jjnic75phgkajfgn --role container-registry.admin --service-account-id $ID_SA
-yc iam access-key create --service-account-id $ID_SA --format json > ~/.ssh/sa-name = "nat-instance"
+yc iam access-key create --service-account-id $ID_SA --format json > ~/.ssh/sa-key.json
 yc iam key create --service-account-id $ID_SA --format json --output ~/.ssh/key.json
 
 ACCESS_KEY_ID=$(grep 'key_id' ~/.ssh/sa-name = "nat-instance" | awk '{print $2}' | tr -d \")
